c31dcf2a06d2656c0b5f98347af7c7b24e3571fd
Gabriel Holanda <gabriel_ebot@hotmail.com>
2019-05-09 11:09:34 -0300

    Position Class

--- src/chess/game/Application.java
+++ src/chess/game/Application.java
@@ -1,6 +1,8 @@
 
 package chess.game;
 
+import boardgame.Position;
+
 /**
  *
  * @author Gabriel Holanda
@@ -9,7 +11,8 @@ public class Application {
 
     
     public static void main(String[] args) {
-        
+        Position  p = new Position(3,5);
+        System.out.println(p.toString());
     }
     
 }
