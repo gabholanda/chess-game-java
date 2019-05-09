be681feae458b211259a26f33bd26c6af7fc2733
Gabriel Holanda <gabriel_ebot@hotmail.com>
2019-05-09 15:00:38 -0300

    Adicionado Programação Defensiva e BoardException

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,4 +1,3 @@
-
 package boardgame;
 
 /**
@@ -6,11 +5,15 @@ package boardgame;
  * @author HOLANDAS
  */
 public class Board {
-    private  int rows;
+
+    private int rows;
     private int columns;
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows < 1 && columns < 1) {
+            throw new BoardException("Erro ao criar tabuleiro: É necessário no mínimo 1 linha e coluna");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -20,28 +23,48 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
     //método que recebe uma linha e coluna e retorna ela no tabuleiro
-    public Piece piece(int row,int column){
+    public Piece piece(int row, int column) {
+        if (!positionExists(row, column)) {
+            throw new BoardException("Posição não está no tabuleiro");
+        }
         return pieces[row][column];
     }
+
     //método que recebe uma posição e retorna ela no tabuleiro - movimentação
-    public Piece piece(Position position){
+    public Piece piece(Position position) {
+        if (!positionExists(position.getRow(), position.getColumn())) {
+            throw new BoardException("Posição não está no tabuleiro");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
+
     // recebe uma peça para colocar no tabuleiro e informa a posição ao piece
-    public void placePiece(Piece piece, Position position){
-       pieces[position.getRow()][position.getColumn()] = piece;
-       piece.position = position;
+    public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position)) {
+            throw new BoardException("Ja há uma peça na posição: " + position);
+        }
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
+
+    //Overloading positionExists
+    private boolean positionExists(int row, int column) {
+        return row >= 0 && row < rows && columns >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position.getRow(), position.getColumn())) {
+            throw new BoardException("Posição não está no tabuleiro");
+        }
+        return piece(position) != null;
     }
 }
---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,15 @@
+
+package boardgame;
+
+/**
+ *
+ * @author HOLANDAS
+ */
+public class BoardException extends RuntimeException {
+    
+    private static final long serialVersionUID = 1L;
+    
+    public BoardException(String msg){
+        super(msg);
+    }
+}
