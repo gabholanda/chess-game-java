5a8be15d9240316ec800ce715400bdea939d81ea
Gabriel Holanda <gabriel_ebot@hotmail.com>
2019-05-14 13:20:31 -0300

    Adicionada lógica de CheckMate

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -18,25 +18,25 @@ public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        List<ChessPiece> captured  = new ArrayList<>();
-        while (true) {
+        List<ChessPiece> captured = new ArrayList<>();
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
-                UI.printMatch(chessMatch,captured);
+                UI.printMatch(chessMatch, captured);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
-                
-                boolean [][] possibleMoves = chessMatch.possibleMoves(source);
+
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
-                UI.printBoard(chessMatch.getPieces(),possibleMoves);
-                
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-                if(capturedPiece != null){
+                if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
             } catch (ChessException | InputMismatchException e) {
@@ -44,6 +44,8 @@ public class Program {
                 sc.nextLine();
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch,captured);
     }
 
 }
--- src/Application/UI.java
+++ src/Application/UI.java
@@ -58,11 +58,17 @@ public class UI {
         System.out.println();
         printCapturedPieces(captured);
         System.out.println("Turn: " + chessMatch.getTurn());
-        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-        if(chessMatch.getCheck()){
-            System.out.println("CHECK!");
+        if (!chessMatch.getCheckMate()) {
+            System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+            if (chessMatch.getCheck()) {
+                System.out.println("CHECK!");
+            }
         }
-        
+        else{
+            System.out.println("CHECKMATE!");
+            System.out.println("Winner: "+ chessMatch.getCurrentPlayer());
+        }
+
     }
 //Método que imprime o tabuleiro e as peças
 
@@ -109,7 +115,7 @@ public class UI {
     private static void printCapturedPieces(List<ChessPiece> captured) {
         List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
         List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
-        
+
         System.out.println("Captured Pieces:");
         System.out.print("White: ");
         System.out.print(ANSI_WHITE);
@@ -118,6 +124,6 @@ public class UI {
         System.out.println("Black: ");
         System.out.println(ANSI_YELLOW);
         System.out.println(Arrays.toString(black.toArray()));
-                
+
     }
 }
